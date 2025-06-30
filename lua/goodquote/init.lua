93d48a6595a94ab40cfa0b2bb229879bad5b719d
James Fraser <wulfgar.pro@gmail.com>
2025-07-01 09:07:47 +1000

    feat(plugin): use opts not config
    Per the lazy.nvim spec: https://lazy.folke.io/spec#spec-setup.

--- lua/goodquote/init.lua
+++ lua/goodquote/init.lua
@@ -5,31 +5,32 @@ local gq = require "gq"
 local curl = require "plenary.curl"
 
 function M.GoodQuote()
-  if M.config and M.config.rss_url then
-    local rss_url = M.config.rss_url
+  if not M.opts or not M.opts.rss_url then
+    print "GoodQuote: `rss_url` not set in opts."
+    return
+  end
 
-    -- Validate the RSS URL.
-    -- For now, just confirm `rss_url` includes the Goodreads quotes RSS URL.
-    if not rss_url:find "https://www.goodreads.com/quotes/list_rss/" then
-      print "GoodQuote: Invalid Goodreads quotes RSS URL."
-      return
-    end
+  local rss_url = M.opts.rss_url
 
-    local rss_xml_response = curl.get(rss_url)
+  -- Validate the RSS URL.
+  -- For now, just confirm `rss_url` includes the Goodreads quotes RSS URL.
+  if not rss_url:find "https://www.goodreads.com/quotes/list_rss/" then
+    print "GoodQuote: Invalid Goodreads quotes RSS URL."
+    return
+  end
 
-    if rss_xml_response.status ~= 200 then
-      print "GoodQuote: Failed to fetch RSS feed."
-      return
-    end
+  local rss_xml_response = curl.get(rss_url)
 
-    local q = gq.gq_parse_rss(rss_xml_response.body)
-    if #q > 0 then
-      print(q[math.random(#q)])
-    else
-      print "GoodQuote: No quotes found."
-    end
+  if rss_xml_response.status ~= 200 then
+    print "GoodQuote: Failed to fetch RSS feed."
+    return
+  end
+
+  local q = gq.gq_parse_rss(rss_xml_response.body)
+  if #q > 0 then
+    print(q[math.random(#q)])
   else
-    print "GoodQuote: `rss_url` not set in configuration."
+    print "GoodQuote: No quotes found."
   end
 end
 
@@ -37,7 +38,7 @@ function M.setup(opts)
   -- Merge user opts with defaults.
   opts = opts or {}
 
-  M.config = opts
+  M.opts = opts
 
   -- Create a user command `:GoodQuote`.
   vim.api.nvim_create_user_command("GoodQuote", M.GoodQuote, {})
