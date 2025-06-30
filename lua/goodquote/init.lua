cd76c742aa0dc6e816ff376ca921bbca99225a38
James Fraser <wulfgar.pro@gmail.com>
2025-07-01 09:40:52 +1000

    feat(plugin): use notify not print

--- lua/goodquote/init.lua
+++ lua/goodquote/init.lua
@@ -6,7 +6,7 @@ local curl = require "plenary.curl"
 
 function M.GoodQuote()
   if not M.opts or not M.opts.rss_url then
-    print "GoodQuote: `rss_url` not set in opts."
+    vim.notify("GoodQuote: `rss_url` not set in opts.", vim.log.levels.ERROR)
     return
   end
 
@@ -15,22 +15,22 @@ function M.GoodQuote()
   -- Validate the RSS URL.
   -- For now, just confirm `rss_url` includes the Goodreads quotes RSS URL.
   if not rss_url:find "https://www.goodreads.com/quotes/list_rss/" then
-    print "GoodQuote: Invalid Goodreads quotes RSS URL."
+    vim.notify("GoodQuote: Invalid Goodreads quotes RSS URL.", vim.log.levels.ERROR)
     return
   end
 
   local rss_xml_response = curl.get(rss_url)
 
   if rss_xml_response.status ~= 200 then
-    print "GoodQuote: Failed to fetch RSS feed."
+    vim.notify("GoodQuote: Failed to fetch RSS feed.", vim.log.levels.ERROR)
     return
   end
 
   local q = gq.gq_parse_rss(rss_xml_response.body)
   if #q > 0 then
-    print(q[math.random(#q)])
+    vim.notify(q[math.random(#q)], vim.log.levels.INFO)
   else
-    print "GoodQuote: No quotes found."
+    vim.notify("GoodQuote: No quotes found.", vim.log.levels.WARN)
   end
 end
 
